f856a2a74c384e372641410795913f5091a1af5b
ramdani221 <ramdani.hidayat221@gmail.com>
2025-04-17 14:48:00 +0700

    tes env

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
-console.log(config)
+console.log(config, process.env)
 const db = {};
 
 let sequelize;
