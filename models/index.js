d9d194f2a84a51b6dcab27c7f2317dabfba44f14
ramdani221 <ramdani.hidayat221@gmail.com>
2025-04-17 17:46:29 +0700

    restore config

--- models/index.js
+++ models/index.js
@@ -6,7 +6,9 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const configPath = path.join(__dirname , '..', 'config', 'config.json')
+console.log(configPath)
+const config = require(configPath)[env];
 const db = {};
 
 let sequelize;
