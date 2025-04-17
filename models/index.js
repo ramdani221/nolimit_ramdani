8f3b40cef6c2549c564d83561d9fee5e05a5f722
ramdani221 <ramdani.hidayat221@gmail.com>
2025-04-17 14:55:34 +0700

    tes variable

--- models/index.js
+++ models/index.js
@@ -8,8 +8,8 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
-console.log(config, process.env)
+const config = require(__dirname + '/../config/config.js')[env];  
+console.log(config, process.env.DB_NAME, process.env.DB_DIALECT)
 const db = {};
 
 let sequelize;
