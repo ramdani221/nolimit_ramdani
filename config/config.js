876070f8b5a143f74ee8e94d1c1942af33f6970d
ramdani221 <ramdani.hidayat221@gmail.com>
2025-04-17 17:25:24 +0700

    try change env

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 const process = require('process');
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    dialect: process.env.DB_DIALECT_DEV,
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    dialect: process.env.DB_DIALECT_TEST,
   },
   production: {
     username: process.env.DB_USERNAME,
